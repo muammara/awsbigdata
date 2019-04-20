706caebad044203835830889fc23bb2702702226
muammara <ahmed_muammar@yahoo.com>
2019-04-20 23:52:19 +0400

    test

--- kinesis/consume.py
+++ kinesis/consume.py
@@ -20,11 +20,14 @@ my_shard_iterator = shard_iterator['ShardIterator']
 record_response = kinesis_client.get_records(ShardIterator=my_shard_iterator,
                                               Limit=2)
 
+n=0
 while 'NextShardIterator' in record_response:
-    record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'],
-                                                  Limit=2)
+    n=n+1
+    record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'],                                                  Limit=2)
+    for x in record_response['Records']:
+        print(x['Data'])
 
-    print(record_response)
+
 
     # wait for 5 seconds
     time.sleep(5)
