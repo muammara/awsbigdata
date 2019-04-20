73ce7914e1201f272334e548c4e5cc0d09e10ef7
muammara <ahmed_muammar@yahoo.com>
2019-04-20 22:07:42 +0400

    test

--- kinesis/rsp-produce.py
+++ kinesis/rsp-produce.py
@@ -8,23 +8,12 @@ from sense_hat import SenseHat
 
 my_stream_name = ''
 rsp_id= 'rsp-home'
-
 kinesis_client = boto3.client('kinesis', region_name='us-east-1')
-def put_to_stream(thing_id, property_value, property_timestamp):
-    """payload = {
-                'prop': str(property_value),
-                'timestamp': str(property_timestamp),
-                'thing_id': thing_id
-              }
-    """
-    payload =read_rsp(rsp_id)
-    print (payload)
 
-    put_response = kinesis_client.put_record(
-                        StreamName=my_stream_name,
-                        Data=json.dumps(payload),
-                        PartitionKey=thing_id)
+def put_to_stream(payload):
+    put_response = kinesis_client.put_record(payload)
     #print(put_response)
+
 def read_rsp(rspname="rsp-home"):
     sense = SenseHat()
     sense.clear()
@@ -45,13 +34,9 @@ def read_rsp(rspname="rsp-home"):
     return(rspenv)
 
 
-
 while True:
-    property_value = random.randint(40, 120)
-    property_timestamp = calendar.timegm(datetime.utcnow().timetuple())
-    thing_id = 'aa-bb'
-
-    put_to_stream(thing_id, property_value, property_timestamp)
-
+    payload =read_rsp(rsp_id)
+    print (payload)
+    put_to_stream(payload)
     # wait for 5 second
     time.sleep(5)
